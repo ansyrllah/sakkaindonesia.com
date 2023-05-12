60f3c0115801b7b0af67e2d23fd08c85d211a0bb
ansyrllah <ansyarullahgo@gmail.com>
2023-05-12 20:43:59 +0700

    revisi mei

--- js/script.js
+++ js/script.js
@@ -1,24 +1,6 @@
-// $(document).ready(function () {
-//     $('.kategori button').click(function () {
-//         var kategori = $(this).data('kategori');
-
-//         // menampilkan gambar produk berdasarkan kategori
-//         if (kategori == 'semua') {
-//             $('.produk').show();
-//         } else {
-//             $('.produk').hide();
-//             $('.kategori-' + kategori).show();
-//         }
-
-//         // menandai tombol yang dipilih
-//         $('.kategori button').removeClass('active');
-//         $(this).addClass('active');
-//     });
-// });
 
 // data
 const jumlahKerah = 14;
-const jumlahFont = 25;
 
 // JENIS KERAH
 // bikin array katalog kerah
@@ -54,36 +36,3 @@ jenisKerahSection.innerHTML = products
     )
     .join(' ');
 
-// JENIS FONT
-// bikin array katalog font
-function getGambarFont(jumlahFont) {
-    const katalogFont = [];
-    if (jumlahFont === 0) {
-        return 0;
-    } else {
-        for (let i = 1; i <= jumlahFont; i++) {
-            const fontItem = {
-                name: `Jenis Font-${i}`,
-                image: `assets/image/jenis-font/jenis-font (${i}).jpg`,
-            };
-            katalogFont.push(fontItem);
-        }
-        return katalogFont;
-    }
-}
-
-// tampilkan semua gambar jenis kerah
-let fonts = getGambarFont(jumlahFont);
-const jenisFontSection = document.getElementById('jenis-font');
-jenisFontSection.innerHTML = fonts
-    .map(
-        (font) => `
-                    <div class="card">
-                        <img src="${font.image}" width="262px" class="card-img-top img-scroll" alt="${font.name}" />
-                        <div class="card-body">
-                            <h5 class="card-title text-capitalize">${font.name}</h5>
-                        </div>
-                    </div>
-                `
-    )
-    .join(' ');
