fdcbd5bab0b78607d3ed84cda4b76cbc0737d511
ansyrllah <ansyarullahgo@gmail.com>
2023-05-26 13:50:20 +0700

    update revisi tombol panah

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ function getGambarKerah(jumlahKerah) {
 
 // tampilkan semua gambar jenis kerah
 let products = getGambarKerah(jumlahKerah);
-const jenisKerahSection = document.getElementById('jenis-kerah');
+const jenisKerahSection = document.getElementById('images-jenis-kerah');
 jenisKerahSection.innerHTML = products
     .map(
         (products) => `
@@ -36,3 +36,271 @@ jenisKerahSection.innerHTML = products
     )
     .join(' ');
 
+
+
+// panah 
+// $(document).ready(function() {
+//     var scrollWrapper = $('.scrolling-wrapper');
+//     var cards = $('.scrolling-wrapper .card');
+//     var cardWidth = cards.first().outerWidth();
+//     var cardMarginRight = parseInt(cards.first().css('margin-right'));
+//     var cardsTotalWidth = (cardWidth + cardMarginRight) * cards.length;
+//     var wrapperWidth = scrollWrapper.outerWidth();
+//     var scrollLeftMax = cardsTotalWidth - wrapperWidth;
+//     var scrollLeftMin = 0;
+  
+//     function scrollCardsLeft(scrollLeft) {
+//       scrollWrapper.animate({
+//         scrollLeft: scrollLeft
+//       }, 500);
+//     } 
+  
+//     $('.scroll-btn.next').click(function() {
+//         var scrollingWrapperId = $(this).data('scrolling-wrapper');
+//         var scrollWrapper = $('#' + scrollingWrapperId + ' .scrolling-wrapper')
+//         var cards = scrollWrapper.find('.card');
+//         var cardWidth = cards.first().outerWidth();
+//         var scrollLeft = scrollWrapper.scrollLeft() + cardWidth + cardMarginRight;
+//         if (scrollLeft > scrollLeftMax) {
+//             scrollLeft = scrollLeftMax;
+//         }
+//         scrollCardsLeft(scrollLeft);
+//         });
+//   });
+
+
+//   $('.scroll-btn.prev').click(function() {
+//     var scrollingWrapperId = $(this).data('scrolling-wrapper');
+//     var scrollWrapper = $('#' + scrollingWrapperId + ' .scrolling-wrapper')
+//     var scrollLeft = scrollWrapper.scrollLeft() - cardWidth - cardMarginRight;
+//     if (scrollLeft < scrollLeftMin) {
+//         scrollLeft = scrollLeftMin;
+//     }
+//     scrollCardsLeft(scrollLeft);
+// });
+
+
+// $(document).ready(function() {
+//     $('.scroll-btn.prev').click(function() {
+//       var scrollWrapper = $(this).closest('.scrolling-wrapper');
+//       var cards = scrollWrapper.find('.card');
+//       var cardWidth = cards.first().outerWidth();
+//       var cardMarginRight = parseInt(cards.first().css('margin-right'));
+//       var scrollLeft = scrollWrapper.scrollLeft() - cardWidth - cardMarginRight;
+//       var scrollLeftMin = 0;
+      
+//       if (scrollLeft < scrollLeftMin) {
+//         scrollLeft = scrollLeftMin;
+//       }
+      
+//       scrollWrapper.animate({
+//         scrollLeft: scrollLeft
+//       }, 500);
+//     });
+  
+//     $('.scroll-btn.next').click(function() {
+//       var scrollWrapper = $(this).closest('.scrolling-wrapper');
+//       var cards = scrollWrapper.find('.card');
+//       var cardWidth = cards.first().outerWidth();
+//       var cardMarginRight = parseInt(cards.first().css('margin-right'));
+//       var scrollLeft = scrollWrapper.scrollLeft() + cardWidth + cardMarginRight;
+//       var wrapperWidth = scrollWrapper.outerWidth();
+//       var cardsTotalWidth = (cardWidth + cardMarginRight) * cards.length;
+//       var scrollLeftMax = cardsTotalWidth - wrapperWidth;
+      
+//       if (scrollLeft > scrollLeftMax) {
+//         scrollLeft = scrollLeftMax;
+//       }
+      
+//       scrollWrapper.animate({
+//         scrollLeft: scrollLeft
+//       }, 500);
+//     });
+//   });
+
+// $(document).ready(function() {
+//     $('.scroll-btn.prev').click(function() {
+//       var scrollingWrapperId = $(this).data('scrolling-wrapper');
+//       var scrollWrapper = $('#' + scrollingWrapperId + ' .scrolling-wrapper');
+//       var cards = scrollWrapper.find('.card');
+//       var cardWidth = cards.first().outerWidth(true);
+//       var cardMarginRight = parseInt(cards.first().css('margin-right'));
+//       var scrollLeft = scrollWrapper.scrollLeft() - cardWidth - cardMarginRight;
+//       var scrollLeftMin = 0;
+  
+//       if (scrollLeft < scrollLeftMin) {
+//         scrollLeft = scrollLeftMin;
+//       }
+  
+//       scrollWrapper.animate({
+//         scrollLeft: scrollLeft
+//       }, 500);
+//     });
+  
+//     $('.scroll-btn.next').click(function() {
+//       var scrollingWrapperId = $(this).data('scrolling-wrapper');
+//       var scrollWrapper = $('#' + scrollingWrapperId + ' .scrolling-wrapper');
+//       var cards = scrollWrapper.find('.card');
+//       var cardWidth = cards.first().outerWidth(true);
+//       var cardMarginRight = parseInt(cards.first().css('margin-right'));
+//       var scrollLeft = scrollWrapper.scrollLeft() + cardWidth + cardMarginRight;
+//       var wrapperWidth = scrollWrapper.outerWidth();
+//       var cardsTotalWidth = (cardWidth + cardMarginRight) * cards.length;
+//       var scrollLeftMax = cardsTotalWidth - wrapperWidth;
+  
+//       if (scrollLeft > scrollLeftMax) {
+//         scrollLeft = scrollLeftMax;
+//       }
+  
+//       scrollWrapper.animate({
+//         scrollLeft: scrollLeft
+//       }, 500);
+//     });
+//   });
+
+// $(document).ready(function() {
+//     $('.scroll-btn.prev').click(function() {
+//       var scrollingWrapperId = $(this).data('scrolling-wrapper');
+//       var scrollWrapper = $('#' + scrollingWrapperId);
+//       var cards = scrollWrapper.find('.card');
+//       var cardWidth = cards.first().outerWidth(); // Gunakan true untuk memperhitungkan margin
+//       var cardMarginRight = parseInt(cards.first().css('margin-right'));
+//       var scrollLeft = scrollWrapper.scrollLeft() - cardWidth - cardMarginRight;
+//       var scrollLeftMin = 0;
+  
+//       if (scrollLeft < scrollLeftMin) {
+//         scrollLeft = scrollLeftMin;
+//       }
+  
+//       scrollWrapper.animate({
+//         scrollLeft: scrollLeft
+//       }, 500);
+//     });
+  
+//     $('.scroll-btn.next').click(function() {
+//       var scrollingWrapperId = $(this).data('scrolling-wrapper');
+//       var scrollWrapper = $('#' + scrollingWrapperId + ' .scrolling-wrapper');
+//       var cards = scrollWrapper.find('.card');
+//       var cardWidth = cards.first().outerWidth(); // Gunakan true untuk memperhitungkan margin
+//       var cardMarginRight = parseInt(cards.first().css('margin-right'));
+//       var scrollLeft = scrollWrapper.scrollLeft() + cardWidth + cardMarginRight;
+//       var wrapperWidth = scrollWrapper.outerWidth();
+//       var cardsTotalWidth = (cardWidth + cardMarginRight) * cards.length;
+//       var scrollLeftMax = cardsTotalWidth - wrapperWidth;
+  
+//       if (scrollLeft > scrollLeftMax) {
+//         scrollLeft = scrollLeftMax;
+//       }
+  
+//       scrollWrapper.animate({
+//         scrollLeft: scrollLeft
+//       }, 500);
+//     });
+//   });
+  
+$(document).ready(function() {
+    $('.scroll-btn.prev').click(function() {
+      var scrollingWrapperId = $(this).data('scrolling-wrapper');
+      var scrollWrapper = $('#' + scrollingWrapperId);
+      var scrollLeft = scrollWrapper.scrollLeft() - scrollWrapper.width()/4;
+      var scrollLeftMin = 0;
+  
+      if (scrollLeft < scrollLeftMin) {
+        scrollLeft = scrollLeftMin;
+      }
+  
+      scrollWrapper.animate({
+        scrollLeft: scrollLeft
+      }, 500);
+    });
+  
+    $('.scroll-btn.next').click(function() {
+      var scrollingWrapperId = $(this).data('scrolling-wrapper');
+      var scrollWrapper = $('#' + scrollingWrapperId);
+      var scrollLeft = scrollWrapper.scrollLeft() + scrollWrapper.width()/4;
+      var scrollWidth = scrollWrapper[0].scrollWidth;
+      var wrapperWidth = scrollWrapper.width();
+      var scrollLeftMax = scrollWidth - wrapperWidth;
+  
+      if (scrollLeft > scrollLeftMax) {
+        scrollLeft = scrollLeftMax;
+      }
+  
+      scrollWrapper.animate({
+        scrollLeft: scrollLeft
+      }, 500);
+    });
+  });
+
+// $(document).ready(function() {
+//     $('.scroll-btn.prev').click(function() {
+//       var scrollingWrapperId = $(this).data('scrolling-wrapper');
+//       var scrollWrapper = $('#' + scrollingWrapperId).find('.scrolling-wrapper');
+//       var scrollLeft = scrollWrapper.scrollLeft() - scrollWrapper.width() / 2;
+//       var scrollLeftMin = 0;
+  
+//       if (scrollLeft < scrollLeftMin) {
+//         scrollLeft = scrollLeftMin;
+//       }
+  
+//       scrollWrapper.animate({
+//         scrollLeft: scrollLeft
+//       }, 500);
+//     });
+  
+//     $('.scroll-btn.next').click(function() {
+//       var scrollingWrapperId = $(this).data('scrolling-wrapper');
+//       var scrollWrapper = $('#' + scrollingWrapperId).find('.scrolling-wrapper');
+//       var scrollLeft = scrollWrapper.scrollLeft() + scrollWrapper.parent().width() / 2;
+//       var scrollWidth = scrollWrapper[0].scrollWidth;
+//       var wrapperWidth = scrollWrapper.width();
+//       var scrollLeftMax = scrollWidth - wrapperWidth;
+  
+//       if (scrollLeft > scrollLeftMax) {
+//         scrollLeft = scrollLeftMax;
+//       }
+  
+//       scrollWrapper.animate({
+//         scrollLeft: scrollLeft
+//       }, 500);
+//     });
+//   });
+
+// $(document).ready(function() {
+//     $('.scroll-btn.prev').click(function() {
+//       var scrollingWrapperId = $(this).data('scrolling-wrapper');
+//       var scrollWrapper = $('#' + scrollingWrapperId).find('.scrolling-wrapper');
+//       var scrollLeft = scrollWrapper.scrollLeft() - scrollWrapper.parent().width() / 2;
+//       var scrollLeftMin = 0;
+  
+//       if (scrollLeft < scrollLeftMin) {
+//         scrollLeft = scrollLeftMin;
+//       }
+  
+//       scrollWrapper.animate({
+//         scrollLeft: scrollLeft
+//       }, 500);
+//     });
+  
+//     $('.scroll-btn.next').click(function() {
+//       var scrollingWrapperId = $(this).data('scrolling-wrapper');
+//       var scrollWrapper = $('#' + scrollingWrapperId).find('.scrolling-wrapper');
+//       var scrollLeft = scrollWrapper.scrollLeft() + scrollWrapper.parent().width() / 2;
+//       var scrollWidth = scrollWrapper[0].scrollWidth;
+//       var wrapperWidth = scrollWrapper.parent().width();
+//       var scrollLeftMax = scrollWidth - wrapperWidth;
+  
+//       if (scrollLeft > scrollLeftMax) {
+//         scrollLeft = scrollLeftMax;
+//       }
+  
+//       scrollWrapper.animate({
+//         scrollLeft: scrollLeft
+//       }, 500);
+//     });
+//   });
+  
+  
+    
+  
+  
